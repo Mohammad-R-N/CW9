b48e6980a0027e3402654e8c4116cbd6b86cabc7
Mohammad-Razzaghi <mohammad.ca@proton.me>
2023-06-02 12:46:55 +0330

    Basic calculator

---
+++ basic calculator/math_operator/multipliction.py
@@ -0,0 +1,3 @@
+def multi(num1:int|float,num2:int|float):
+    """return the multipliction of two numbers."""
+    return num1*num2
---
+++ basic calculator/math_operator/subtraction.py
@@ -0,0 +1,3 @@
+def sub(num1:int|float,num2:int|float):
+    """return the subtraction of two numbers."""
+    return num1-num2
