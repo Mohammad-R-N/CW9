bf457f6472381c6cd1ba741efc218709d42619c0
Mr-Coder <mohammad.ca@proton.me>
2023-06-22 07:57:03 -0400

    Q1 part5

--- cw12/Q1.sql
+++ cw12/Q1.sql
@@ -20,3 +20,8 @@ From film INNER JOIN film_category ON film_category.film_id = film.film_id
 INNER JOIN category ON category.category_id=film_category.category_id
 group by name
 HAVING count(*) >60 AND count(*)<68;
+
+--part #5
+SELECT language.name language,film.title,category.name category
+From film INNER JOIN language ON language.language_id=film.language_id
+INNER  JOIN film_category ON film_category.film_id = film.film_id;
