603ca1f4d6a6236c29e487ee6a9b659fd1c8d2c7
Mr-Coder <mohammad.ca@proton.me>
2023-06-22 07:53:09 -0400

    Q1 part4

--- cw12/Q1.sql
+++ cw12/Q1.sql
@@ -13,3 +13,10 @@ SELECT ca.name category ,count(*) From file c
 INNER JOIN film_category fc ON fc.film_id= c.film_id
 INNER JOIN category ca ON ca.category_id =fc.category_id
 group by 1;
+
+--part #4
+SELECT name,count(*) as count
+From film INNER JOIN film_category ON film_category.film_id = film.film_id
+INNER JOIN category ON category.category_id=film_category.category_id
+group by name
+HAVING count(*) >60 AND count(*)<68;
