4fe73456edfff9f83dd9388a220dceb545363645
Mr-Coder <mohammad.ca@proton.me>
2023-06-22 07:50:12 -0400

    Q1 part2

--- cw12/Q1.sql
+++ cw12/Q1.sql
@@ -3,7 +3,10 @@ SELECT title, release_year, category_id
 From file INNER JOIN film_category on film_category.film_id = film.film_id;
 
 --part #2
-
+SELECT title, release_year, name
+From film INNER JOIN film_category ON film_category.film_id=film.film_id
+INNER JOIN category ON category.category_id = film_category.category_id
+WHERE name IN ('Action','Comedy','Family');
 
 --part #3
 SELECT ca.name category ,count(*) From file c
