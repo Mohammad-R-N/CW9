06534e305c5f600f73a044925b2201841baa1855
Mr-Coder <mohammad.ca@proton.me>
2023-06-22 07:46:19 -0400

    Q1 part3

--- cw12/Q1.sql
+++ cw12/Q1.sql
@@ -1,3 +1,12 @@
 --part #1
 SELECT title, release_year, category_id
 From file INNER JOIN film_category on film_category.film_id = film.film_id;
+
+--part #2
+
+
+--part #3
+SELECT ca.name category ,count(*) From file c
+INNER JOIN film_category fc ON fc.film_id= c.film_id
+INNER JOIN category ca ON ca.category_id =fc.category_id
+group by 1;
